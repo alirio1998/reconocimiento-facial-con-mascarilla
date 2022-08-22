794ef439cbb74fc9940e989c696dc4a05e05a9ca
alirio1998 <62248516+alirio1998@users.noreply.github.com>
2022-08-22 18:16:31 -0400

    Update entrenarModelo.js

--- entrenarModelo.js
+++ entrenarModelo.js
@@ -1,32 +1,84 @@
-// Copyright (c) 2019 ml5
-//
-// This software is released under the MIT License.
-// https://opensource.org/licenses/MIT
-
-/* ===
-ml5 Example
-Image Classification using Feature Extraction with MobileNet. Built with p5.js
-This example uses a callback pattern to create the clasificador
-=== */
 
 let featureExtractor;
 let clasificador;
-let video;
-let loss;
+let image
+let canvas
+var idImg = 0
+var modeloCargado = false;
+var imageUpload = document.getElementById('stream')
 let imgMascarilla=0;
 let imgSinMascarilla=0;
 let imgNadie=0;
+var imageFace = document.getElementById('face')
+const container = document.getElementById('contenedorVideo')
+const divLoading = document.getElementById('contenedorLoading')
+const divContenido = document.getElementById('contenido')
+divContenido.className = "hidden";
+function setup() {
+  imageUpload.src=''+document.location.origin+'/stream';
+  imageUpload.onload=imagenEncontrada;
+  imageUpload.onloadstart = BuscandoImagen;
+  imageUpload.onerror=imageNoEncontrada;
+}
+async function cargarTensorflow() {
+  
+    noCanvas();
+    featureExtractor = ml5.featureExtractor("MobileNet", modeloListo);
+    const options = { numLabels: 2 };
+    clasificador = featureExtractor.classification();
 
-async function faceDetect() {
-  /*
-  imageUpload.src='images/con_mascarilla/asian_mask'+idImg+'.jpg';
-  idImg++;
-  if(idImg>200){
-    idImg = 1;
+    cargarFaceApi();
+    cargarBotones();
+}
+function cargarFaceApi(){
+  Promise.all([
+    faceapi.nets.faceRecognitionNet.loadFromUri('https://alirio1998.github.io/reconocimiento-facial-con-mascarilla/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('https://alirio1998.github.io/reconocimiento-facial-con-mascarilla/models'),
+    faceapi.nets.ssdMobilenetv1.loadFromUri('https://alirio1998.github.io/reconocimiento-facial-con-mascarilla/models'),
+    setTimeout(function(){}, 3000)
+  ]).then(setInterval(InicioDeteccionFacial, 1))
+}
+
+async function inicioFaceApi() {
+
+
+  if (!imageUpload.complete) {
+
   }
-  */
-  //imageUpload.src='https://alirio1998.github.io/reconocimiento-facial-con-mascarilla/images/desconocido.jpg';
-    image = video;
+  else { 
+    console.log('Cargando Imagen');
+  }
+
+  /*
+    if (canvas) canvas.remove()
+    image = document.getElementById('stream')
+    canvas = faceapi.createCanvasFromMedia(image)
+    const displaySize = { width: image.width, height: image.height }
+    faceapi.matchDimensions(canvas, displaySize)
+    const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    console.log(detections);
+    resizedDetections.forEach(detection => {
+      const box = detection.detection.box
+      extractFaceFromBox(image, detection.detection.box)
+      const drawBox = new faceapi.draw.DrawBox(box)
+      drawBox.draw(canvas)
+    })
+    */
+}
+async function InicioDeteccionFacial() {
+
+    //has picture
+    console.log('inicio deteccion');
+
+    idImg++;
+    //imageUpload.src='/images/con_mascarilla/asian_mask'+idImg+'.jpg';
+    ;
+    if(idImg>200){
+      idImg = 1;
+    }
+    
+    image = imageUpload;
     container.append(image);
     const detections = await faceapi.detectAllFaces(image).withFaceLandmarks();
 
@@ -35,168 +87,133 @@ async function faceDetect() {
     if(results.length>0){
       results.forEach((result, i) => {
         console.log(detections[0].detection.box);
-        extractFaceFromBox(image, detections[0].detection.box);
+        
+        ObtenerRostro(image, detections[0].detection.box);
+        
+          
+      
+        
         //const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
         //drawBox.draw(canvas)
       })
     }else{
       const canvas = document.getElementById('face');
-      canvas.src = 'https://alirio1998.github.io/reconocimiento-facial-con-mascarilla/images/desconocido.jpg';
+      imageFace.src = 'https://alirio1998.github.io/reconocimiento-facial-con-mascarilla/images/desconocido.jpg';
+      //document.body.append(canvas) ;
+      select("#resultado").html('Ninguna persona detectada');
+      select("#coincidencia").html(`100%`);
+      var elemento = document.getElementById('stream');
+      elemento.className = "";
+        
+
       
-      videoDetect.append(canvas);
+      //videoDetect.append(canvas);
     }
     console.log(results);
-  
-};
-
-async function extractFaceFromBox(inputImage, box){ 
-
-  const regionsToExtract = [
-      new faceapi.Rect( box.x, box.y , box.width , box.height)
-  ]
-                      
-  let faceImages = await faceapi.extractFaces(inputImage, regionsToExtract)
-  
-  if(faceImages.length == 0){
-      console.log('Face not found')
-  }
-  else
-  {
-    const canvas = document.getElementById('face');
-      faceImages.forEach(cnv =>{      
-
-          canvas.src = cnv.toDataURL();
-              
-      })
-      videoDetect.append(canvas);
-  }   
-}    
-
-function imageReady() {
-  image(video, 0, 0, width, height);
-}
-function setup() {
-   noCanvas();
-  // Crea el video
-  console.log(document.location.origin);
- // video = createCapture(VIDEO);
-  //video.parent("contenedorVideo");
-  video = document.getElementById('stream');
-  video.src=''+document.location.origin+':81/stream';
-  // extrae modelo MobileNet
-  featureExtractor = ml5.featureExtractor("MobileNet", modeloListo);
-
-  // crea un nuevo calificador
-  const options = { numLabels: 2 };
-  clasificador = featureExtractor.classification();
-  // activa los botones
-  botones();
 }
 function modeloListo() {
-    
-    // modelos precargados
-    clasificador.load('https://alirio1998.github.io/reconocimiento-facial-con-mascarilla/models/model.json', customModelReady);
-    var elemento = document.querySelector('#contenidoBotones');
-    elemento.className = "show";
-
-    
+  // modelos precargados
+  clasificador.load('https://alirio1998.github.io/reconocimiento-facial-con-mascarilla/models/model.json', function(){
+    clasificar();
+  });
+  console.log('modelo cargado');
 }
-
- function customModelReady() {
-        select("#estadoModelo").html("Modelo cargado!");
-        clasificar();
-    };
 function videoListo() {
-    select("#estadoVideo").html("Video cargado!");
+  console.log('Imagen cargada');
 }
 
+function imagenEncontrada() {
+  console.log('Imagen Encontrada');
+  
+  imageUpload.className = "";
+  divLoading.className = "hidden";
+  divContenido.className = "";
+  cargarTensorflow();
+}
+function imageNoEncontrada() {
 
-// Clasificar el resultado
-function clasificar() {
-        const img = document.querySelector('img');
-        clasificador.classify(img, gotResults);
+  console.log('Imagen no encontrada');
+}
+function BuscandoImagen() {
+  console.log('Imagen no encontrada');
 }
 
-// funciones de botones
-function botones() {
+function cargarBotones() {
     var botonSin = select("#btnNoMascarilla");
     botonSin.mousePressed(function () {
-      const img = document.querySelector('img');
-        clasificador.addImage(img, "sin_mascarilla");
-        select("#sumaSinMascarilla").html((imgSinMascarilla += 1));
+        clasificador.addImage(imageFace,"sin_mascarilla");
+        select("#sumaSinMascarilla").html(imgSinMascarilla++);
     });
     var botonCon = select("#btnConMascarilla");
     botonCon.mousePressed(function () {
-      const img = document.querySelector('img');
-        clasificador.addImage(img, "con_mascarilla");
-        select("#sumaConMascarilla").html((imgMascarilla += 1));
+        clasificador.addImage(imageFace,"con_mascarilla");
+        select("#sumaConMascarilla").html(imgMascarilla++);
     });
-  /*
-    var botonNadie = select("#btnNadie");
-    botonNadie.mousePressed(function () {
-      const img = document.querySelector('img');
-        clasificador.addImage(img, "nadie");
-        select("#nadie").html((imgNadie += 1));
+
+    
+    var botonEntrenar = select("#btnEntrenar");
+    botonEntrenar.mousePressed(function () {
+        clasificador.train(function(vError){
+            if(vError){
+                error = vError;
+                select("#error").html("Error "+error);
+            }else{
+                select("#error").html("Entrenamiento terminado con un error de "+error);
+
+            }
+        });
     });
-*/
-  // entrenamiento del modelo
-  var botonEntrenar = select("#btnEntrenar");
-  botonEntrenar.mousePressed(entrenar);
-
-  // Boton predecir
-  var botonPredecir = select("#btnPredecir");
-  botonPredecir.mousePressed(clasificar);
-
-  // Guardar modelo
-  var guardarBtn = select("#btnGuardar");
-  guardarBtn.mousePressed(function() {
-    clasificador.save("https://alirio1998.github.io/reconocimiento-facial-con-mascarilla/models/");
-  });
-
-
-/*
-  // Load model
-  var loadBtn = select("#load");
-  loadBtn.changed(function() {
-    clasificador.load(loadBtn.elt.files, function() {
-      select("#modelStatus").html("Custom Model Loaded!");
+    var botonPredecir = select("#btnPredecir");
+    botonPredecir.mousePressed(function () {
+        clasificador.classify(muestraResultado);
     });
-  });
-  */
+
 }
-
-async function entrenar(mobilenetModule) {
-
-  clasificador.train(function(perdida) {
-    if (perdida) {
-      error = perdida;
-      select("#error").html("Perdida "+error);
-    } else {
-      select("#error").html("Entrenamiento terminado con perdida de "+error);
+async function ObtenerRostro(inputImage, box){ 
+    const regionsToExtract = [
+        new faceapi.Rect( box.x, box.y , box.width , box.height)
+    ]
+                        
+    let faceImages = await faceapi.extractFaces(inputImage, regionsToExtract)
+    
+    if(faceImages.length == 0){
+        console.log('Face not found')
     }
-  });
-
+    else
+    {
+      const canvas = document.getElementById('face');
+        faceImages.forEach(cnv =>{      
+  
+            imageFace.src = cnv.toDataURL();
+                
+        })
+        //document.body.append(canvas)  
+    }   
 }
-// Muestra los resultados
-function gotResults(err, results) {
+// Clasificar el resultado
+function clasificar() {
+    clasificador.classify(imageFace, Resultados);
+}
+function Resultados(err, results) {
   // muestra los errores
   if (err) {
     console.log(err);
   }
   if (results && results[0]) {
+    //setTimeout(startDetection, 3000);
     console.log(results);
     var val = Math.trunc(results[0].confidence.toFixed(2) * 100 );
     select("#resultado").html(results[0].label);
     select("#coincidencia").html(`${val }%`);
-    var elemento = document.querySelectorAll("img");
+    var elemento = document.getElementById('stream');
     if (results[0].label == "sin_mascarilla") { // sin mascarilla bode rojo
-      elemento[0].className = "sin-mascarilla";
+      elemento.className = "sin-mascarilla";
     } else if(results[0].label == "con_mascarilla"){ // con mascarilla borde verde
-      elemento[0].className = "con-mascarilla";
+      elemento.className = "con-mascarilla";
     } else if(results[0].label == "nadie"){ // con mascarilla borde verde
-      elemento[0].className = "nadie";
+      elemento.className = "nadie";
     }
-
-    clasificar();
   }
 }
+  
+  
